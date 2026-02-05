642ae38e9606dfade3de5bc4b9ddc8a539d3e83e
Andrés González <0255690@up.edu.mx>
2026-02-05 07:56:54 -0600

    Actividad 1 terminada

--- Template/js/main.js
+++ Template/js/main.js
@@ -1,4 +1,18 @@
-/*
-*    main.js
-*/
+var svg = d3.select("#chart-area").append("svg")
+    .attr("width", 600)
+    .attr("height", 600);
 
+// Circle
+var circle = svg.append("circle")
+    .attr("cx", 100)
+    .attr("cy", 250)
+    .attr("r", 100)
+    .attr("fill", "blue");
+
+// Rectangle
+var rect = svg.append("rect")
+    .attr("x", 20)
+    .attr("y", 20)
+    .attr("width", 50)
+    .attr("height", 70)
+    .attr("fill", "orange");
